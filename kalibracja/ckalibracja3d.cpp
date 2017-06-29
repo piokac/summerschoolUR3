81c4938840d83ca4e664662060a7169784eed72e
krzysztof.marchewka <krzysztof.marchewkaa@gmail.com>
2017-06-29 23:23:26 +0200

    cc

--- kalibracja/ckalibracja3d.cpp
+++ kalibracja/ckalibracja3d.cpp
@@ -5,3 +5,17 @@ cKalibracja3d::cKalibracja3d()
 
 }
 
+
+
+void cKalibracja3d::kalibruj()
+{
+    //wiza
+}
+
+QVector<punktTCP> cKalibracja3d::transformuj(QVector<punkt> wektor)
+{
+    for(auto itr=wektor.begin();itr<wektor.end(); itr++)
+    {
+        wektor[itr]=kalibruj3D(wektor[itr]);
+    }
+}
